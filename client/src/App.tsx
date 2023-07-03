c8a0eb09be4f575502551496f114640ec4d2c950
hktrib(Apramay Singh) <hktribunal@gmail.com>
2023-07-03 10:35:48 -0700

    Client Side -> adding components

---
+++ client/src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import Sidebar from './Sidebar';
+
+const App = () => {
+  return (
+    <div className="App">
+      <Sidebar/>
+      <h1>Aksara Nitai Dasa is starting this journey</h1>
+    </div>
+  );
+}
+
+export default App;
---
+++ client/src/Sidebar.tsx
@@ -0,0 +1,10 @@
+
+import './Sidebar.css'
+const Sidebar = () => {
+    return (
+        <div className="sidebar">
+            <h1>Sidebar is here</h1>
+        </div>
+    );
+}
+export default Sidebar;
